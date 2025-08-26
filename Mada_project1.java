c0ca001c547a392e80de41aeff360fe116157b91
ahmadyamaulana <169001210+ahmadyamaulana@users.noreply.github.com>
2025-08-26 23:09:12 +0700

    Add files via upload

---
+++ Mada_project1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mada_project1;
+
+/**
+ *
+ * @author Maullovers
+ */
+public class Mada_project1 {
+
+    public static void main(String[] args) {
+        String nama ="Ahmada Maulana";
+        String npm ="2440506069";
+        String alamat ="wates Beningan Magelang";
+        String no_hp ="085725734175";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("NPM : " + npm);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("No HP : " + no_hp);
+    }
+}
