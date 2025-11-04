bb219b42e0fbbf001a049ea13d9d770a454303dc
ahmadyamaulana <169001210+ahmadyamaulana@users.noreply.github.com>
2025-11-04 16:12:58 +0700

    Create Hewan.java

---
+++ Minggu9/Hewan.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author Acer
+ */
+abstract class Hewan {
+    // Metode abstrak
+ abstract void Suara();
+ // Metode dengan implementasi
+ void info() {
+ System.out.println("Ini adalah Suara Hewan.");
+ }
+}
