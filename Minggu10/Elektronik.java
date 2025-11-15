90eb4d1d549b04a31b8637df337c9d704844625a
ahmadyamaulana <169001210+ahmadyamaulana@users.noreply.github.com>
2025-11-15 16:52:54 +0700

    Add Elektronik class implementing Pembayaran interface

---
+++ Minggu10/Elektronik.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikumpbo_10;
+
+/**
+ *
+ * @author Maullovers
+ */
+public class Elektronik implements Pembayaran {
+    @Override
+    public double hitung(double harga) {
+        return harga * 0.10; // contoh pajak 10%
+    }
+}
+
