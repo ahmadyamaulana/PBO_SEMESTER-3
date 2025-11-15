e09fec668c565e0026087168b69f4d274704cdcd
ahmadyamaulana <169001210+ahmadyamaulana@users.noreply.github.com>
2025-11-15 16:14:08 +0700

    Add OperasiHitung interface for calculations

---
+++ Minggu10/OperasiHitung.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikumpbo_10;
+
+/**
+ *
+ * @author Maullovers
+ */
+public interface OperasiHitung {
+    int hitung(int a, int b);
+}
