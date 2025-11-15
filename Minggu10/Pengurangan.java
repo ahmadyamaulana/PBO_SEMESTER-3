da898b366ae519103253b0556b10d484c857ee03
ahmadyamaulana <169001210+ahmadyamaulana@users.noreply.github.com>
2025-11-15 16:14:58 +0700

    Add Pengurangan class for subtraction operation

---
+++ Minggu10/Pengurangan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikumpbo_10;
+
+/**
+ *
+ * @author Maullovers
+ */
+public class Pengurangan implements OperasiHitung {
+    @Override
+    public int hitung (int a, int b) {
+        return a - b;
+    }
+}
